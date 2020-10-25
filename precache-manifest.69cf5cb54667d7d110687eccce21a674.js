49822f5560b572c4c26a4d61c7ddbbb50ae932e7
Edward <e.arechwa@gmail.com>
2020-10-25 18:52:42 +0100

    add

---
+++ precache-manifest.69cf5cb54667d7d110687eccce21a674.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18ccc4687f9aa4c7e6a12de123e325cb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "35809888c40b2bea8a3f",
+    "url": "./static/css/main.3f4f726f.chunk.css"
+  },
+  {
+    "revision": "590cf573d3da54d0b49a",
+    "url": "./static/js/2.b7dc2998.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.b7dc2998.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35809888c40b2bea8a3f",
+    "url": "./static/js/main.55407914.chunk.js"
+  },
+  {
+    "revision": "d76db9546e27ceeeb417",
+    "url": "./static/js/runtime-main.c0297098.js"
+  },
+  {
+    "revision": "570a244cacd3d78b8c75ac5dd622f537",
+    "url": "./static/media/Montserrat-ExtraLight.570a244c.ttf"
+  },
+  {
+    "revision": "c57e537a1c25ff6c5beb211ef1c86634",
+    "url": "./static/media/PoiretOne-Regular.c57e537a.ttf"
+  }
+]);
